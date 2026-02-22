77718e94a55ee7184a8459500b1481d8f2a7a706
root <root@Catermm.localdomain>
2026-02-22 02:40:09 +0100

    Modified rank and file schema to better match data structure

--- models/rankAndFile.js
+++ models/rankAndFile.js
@@ -1,10 +1,35 @@
 const mongoose = require('mongoose');
-const User = require('./User');
 
 const rankAndFileSchema = new mongoose.Schema({
-	userId: { type: String, required: true, unique: true },
-	roleId: { type: String, required: true, unique: true },
-	description: { type: String, default: 'No description provided.' },
+	// Company array
+	companies: [
+		{
+			companyId: { type: String, required: true, unique: true },
+			companyName: { type: String, required: true, unique: true },
+			// Squads within the company
+			squads: [
+				{
+					squadId: { type: String, required: true, unique: true },
+					squadName: { type: String, required: true, unique: true },
+					squadMemberCount: {
+						type: Number,
+						required: true,
+						default: 0,
+						min: 0,
+						max: 10,
+					},
+					squadMembers: [
+						{
+							userId: { type: String, required: true },
+							username: { type: String, required: true },
+							rank: { type: String, required: true },
+							dateJoinedSquad: { type: Date, default: Date.now },
+						},
+					],
+				},
+			],
+		},
+	],
 });
 
 module.exports = mongoose.model('RankAndFile', rankAndFileSchema);
